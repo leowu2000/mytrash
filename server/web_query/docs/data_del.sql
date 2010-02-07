1cdd30b91c8d5142f094dd68fc97b1c8d796a7f8
leowu2000 <leowu2000@8bb85e52-ee1e-11de-a5fe-b7516cc0fd57>
2010-02-07 07:34:43 +0000

    清空数据  不清空字典表

---
+++ server/web_query/docs/data_del.sql
@@ -0,0 +1,17 @@
+delete from TB_FXJB_M;
+delete from TB_FXJB;
+delete from TB_FPACTI_M;
+delete from TB_FPACTI;
+delete from TB_QT_M;
+delete from TB_QT;
+delete from TB_SD_M;
+delete from TB_SD;
+
+delete from TB_RSR;
+delete from TB_PJR_M;
+delete from TB_PJRCN;
+delete from TB_BURDSC;
+delete from TB_STDNC_M;
+delete from TB_STDNC;
+delete from TB_ST;
+delete from TB_PJ;
