02cd6e17d3b9a7e0f0df210dd7bedc878d292f57
leowu2000 <leowu2000@8bb85e52-ee1e-11de-a5fe-b7516cc0fd57>
2010-03-01 09:28:03 +0000

    网络传输测试

---
+++ server/web_query/src/com/basesoft/server/Client.java
@@ -0,0 +1,14 @@
+package com.basesoft.server;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.send("d:/aaa.zip");
+	}
+
+	private void send(String string) {
+		
+	}
+
+}
