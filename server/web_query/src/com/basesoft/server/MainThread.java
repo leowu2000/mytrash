e56b550d1e3b595337247bb125edbfc4e68fcca9
leowu2000 <leowu2000@8bb85e52-ee1e-11de-a5fe-b7516cc0fd57>
2010-03-03 16:25:22 +0000

    删除及代码重构

--- server/web_query/src/com/basesoft/server/MainThread.java
+++ server/web_query/src/com/basesoft/server/MainThread.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
 
-public class MainThread extends Thread{
+public class MainThread extends Thread {
 	@Override
 	public void run() {
 		ServerSocket rServer = null;
@@ -16,10 +16,6 @@ public class MainThread extends Thread{
 			p.load(this.getClass().getResourceAsStream("/config.properties"));
 			int default_port = Integer.parseInt(p.getProperty("Port"));
 			rServer = new ServerSocket(default_port);
-			System.out.println("The server is ready!Port: " + default_port);
-//super.ServerListPort("欢迎使用实时工情数据接收服务");
-//super.ServerListPort("服务已经开始!!端口：" + default_port);
-//System.out.println("Thread number: " + super.ConnectNum);
 			while (true) {
 				request = rServer.accept();
 				System.out.println("sock is connected");
