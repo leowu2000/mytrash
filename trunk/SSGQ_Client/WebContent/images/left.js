b800fa03c128b8da2b0a2c3c9d5bb329245d80e9
leowu2000 <leowu2000@8bb85e52-ee1e-11de-a5fe-b7516cc0fd57>
2010-03-28 14:12:01 +0000

    首页面上方导航按钮

--- trunk/SSGQ_Client/WebContent/images/left.js
+++ trunk/SSGQ_Client/WebContent/images/left.js
@@ -19,10 +19,10 @@ function switchTo(which){
 	var navli="#menu-nav .nav"+which;
 	var nava="#menu-nav .nav"+which+" a";
 	var navspan="#menu-nav .nav"+which+" span";
-	$("#menu-nav span").animate({opacity: '.65'}, { "duration": 300 } );	
+	$("#menu-nav span").animate({opacity: '.65'}, { "duration": 200 } );	
 	$("#menu-nav a").css("cursor", "pointer");		
 	$(nava).css("cursor", "default");			
-	$("#menu-nav .current").animate({top: (which-1)*36+"px"  }, { "duration": 300 } );
-	$(navspan).animate({opacity: '1.0'}, { "duration": 300 } );
+	$("#menu-nav .current").animate({top: (which-1)*36+"px"  }, { "duration": 200 } );
+	$(navspan).animate({opacity: '1.0'}, { "duration": 200 } );
 	curSect = which;
 }
