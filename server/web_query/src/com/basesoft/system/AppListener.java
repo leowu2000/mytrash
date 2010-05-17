d8805a2021d5426f9ea186d6411cdb2ea6703c5a
leowu2000 <leowu2000@8bb85e52-ee1e-11de-a5fe-b7516cc0fd57>
2010-05-17 12:51:48 +0000

    数据库自动更新

--- server/web_query/src/com/basesoft/system/AppListener.java
+++ server/web_query/src/com/basesoft/system/AppListener.java
@@ -5,8 +5,11 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.basesoft.server.MainThread;
+import com.basesoft.util.DBUpgrade;
 
 public class AppListener implements ServletContextListener {
 	protected static Log logger = LogFactory.getLog(AppListener.class);
@@ -14,10 +17,17 @@ public class AppListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.println("系统根目录：" + sce.getServletContext().getRealPath(""));
 
-		// WebApplicationContext context =
-		// WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
-		// DBUpgrade dbUpgrade = (DBUpgrade) context.getBean("dbUpgrade");
-		// dbUpgrade.upgrade(Consts.ROOTPATH + "/WEB-INF/dbupgrade/");
+		try {
+			WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
+			DBUpgrade dbUpgrade = (DBUpgrade) context.getBean("dbUpgrade");
+
+			sce.getServletContext().setAttribute("RootPath", sce.getServletContext().getRealPath(""));
+			sce.getServletContext().setAttribute("springContext",
+					WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext()));
+			dbUpgrade.upgrade(sce.getServletContext().getAttribute("RootPath") + "/WEB-INF/dbupgrade/");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
 		Thread s = new MainThread();
 		s.setDaemon(true);// 设置线程为后台线程，使tomcat重启的时候自动退出。
